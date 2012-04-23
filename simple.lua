b577eb7878925991466c1c57efdd53576c0bfee9
Jim Bauwens <jimbauwens@gmail.com>
2012-04-23 11:37:54 +0200

    added comments, so you can understand what is happening

--- simple.lua
+++ simple.lua
@@ -42,6 +42,7 @@ function simplify(rpn)
 	return rpn
 end
 
+-- Create a new RPN valid group from data values. startgroup ('a') is need to fix the previous RPN group
 function creategroup(datatable, operator, startgroup)
 	local group	=	{}
 	
@@ -55,22 +56,27 @@ function creategroup(datatable, operator, startgroup)
 	return group
 end
 
+-- Simplify the group. If startgroup is not nil, then it is part of a previous RPN group
 function simpgroup(rpn, posa, posb, o, startgroup)
 	local len	= #rpn
 	local n	= posb-posa		-- The length of the RPN group we are handling
 	local datatable	= {}
 	
+	
+	-- Remove the ENTIRE (and possible the last operator of the previous RPN group) and put the datavalues in a sepperate table
 	local d
 	for i=posa, posb do
-		d	= table.remove(rpn, posa) -- Remove the whole group from the RPN table
+		d	= table.remove(rpn, posa)
 		
 		if not operator[d] then 
 			table.insert(datatable, d)
 		end
 	end
 	
+	-- Sort the table, so we can easily simplify it
 	table.sort(datatable)	-- sort the datatable
 	
+	-- combine numbers together
 	local n1, n2
 	if o ~= "/" then
 		while tonumber(datatable[1]) and tonumber(datatable[2]) do
@@ -78,42 +84,18 @@ function simpgroup(rpn, posa, posb, o, startgroup)
 		end
 	end
 	
+	-- create a RPN valid group of the remaining data values
 	local group	= creategroup(datatable, o, startgroup)
 	
+	-- Reinsert the group into our RPN table
 	for k, value in ipairs(group) do
 		table.insert(rpn, posa+k-1, value)
 	end
 	
+	-- Return true if the size changed
 	if len<#rpn then 
 		return true
 	end
-end
-
-function sortgroup(rpn, posa, posb, startgroup)
-	local sorttable	= {}
-	if startgroup then
-		table.insert(sorttable, rpn[posa])
-	end
-	
-	table.insert(sorttable, rpn[posa+1])
-	for i=posa+3, posb, 2 do
-		table.insert(sorttable, rpn[i])
-	end
-	table.sort(sorttable)
-
-
-	local j	= 1
-	if startgroup then	
-		rpn[posa]	= sorttable[j]
-		j	= j+1
-	end
-	
-	rpn[posa+1]	= sorttable[j]
-	
-	for i=posa+3, posb, 2 do
-		j	= j+1
-		rpn[i]	= sorttable[j]
-	end	
 end
 
 function findgroup(rpn, pos, ro)
@@ -143,7 +125,6 @@ function sortit(rpn)
 	local len	= #rpn
 	local cgroup	= {}
 	local breakuntil	= 0
-	local sortgroup
 	
 	for i=1, len-2 do 
 		if breakuntil>i then 
@@ -153,9 +134,23 @@ function sortit(rpn)
 		local a	= rpn[i]
 		local b	= rpn[i+1]
 		local o	= rpn[i+2]
-		if not operator[b] and operator[o] then -- this means for example "5 5 *"
+		if not operator[b] and operator[o] then
+		
+			-- Alright, we look for the start of a new group. 'a' CAN be an operator of a group before that.
+			-- We use it determine the group type we detected. We may never lose 'a', as it is needed for correct RPN
+			-- Example group if 'a' is an operator:
+			-- * 9 + 6 +
+			-- If 'a' is not an operator
+			-- 8 9 + 6 +
+			-- We can simplify both groups, but we need to take 'a' in account
+			
+			-- Find the group
 			local posb	= findgroup(rpn, i, o)
+			
+			-- Simplify (if it can) the group.
 			local done	= simpgroup(rpn, i, posb, o, operator[a] and a)
+			
+			-- If there are changes, try to simplify it again. This is currently recursive, should change to non-recursive.
 			if done then
 				sortit(rpn)
 				break
@@ -165,6 +160,9 @@ function sortit(rpn)
 	
 	return rpn
 end
+
+
+
 
 function cc(tbl)
 	out	= ""
